257bf66142e5eb3fac09624fa922ec8a6ef28862
Kritika Siraswa <ksiraswa9481@gmail.com>
2026-02-08 11:03:32 +0530

    abcd

---
+++ .dist/conditions.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class conditions {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.=");
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("The number is even");
+            }
+            else {
+                System.out.println("The number is odd");
+                }
+                sc.close();
+            }
+}
